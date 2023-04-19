dafa15fe377db459988a86a06df3aeaeb1ee07e8
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-04-19 12:06:52 -0700

    changing it up

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -5,14 +5,14 @@ import { createClient } from 'graphql-ws';
 import { getSession } from 'next-auth/react';
 
 const httpLink = new HttpLink({
-   uri: 'https://next-chat-backend.herokuapp.com/',
+   uri: 'https://next-chat-backend.herokuapp.com/graphql',
    credentials: 'include',
 })
 
 
 
 const wsLink = typeof window !== 'undefined' ? new GraphQLWsLink(createClient({
-   url: 'ws://next-chat-backend.herokuapp.com/subscriptions',
+   url: 'ws://next-chat-backend.herokuapp.com/graphql/subscriptions',
    connectionParams: async () => ({
       session: await getSession()
    })
