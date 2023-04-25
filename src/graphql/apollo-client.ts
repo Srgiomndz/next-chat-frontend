6d6f3934cb8e2f092e4f3d333f69f7dad2ad3a23
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-04-25 11:54:44 -0700

    fixes

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -5,7 +5,7 @@ import { createClient } from 'graphql-ws';
 import { getSession } from 'next-auth/react';
 
 const httpLink = new HttpLink({
-   uri: 'https://next-chat-backend.herokuapp.com/',
+   uri: 'https://next-chat-backend.herokuapp.com/graphql',
    credentials: 'include',
 })
 
